bc53110fddb0ba94a7e6100cbad4e6be62da3da8
jon <jcrossguitar@gmail.com>
2019-09-01 08:27:45 -0700

    I was able to create a search variable that takes in arguments and finds a search term

--- key.js
+++ key.js
@@ -5,13 +5,30 @@
 
 
 var Spotify = require('node-spotify-api');
+
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var search = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    search = search + "+" + nodeArgs[i];
+  } else {
+    search += nodeArgs[i];
+
+  }
+}
  
 var spotify = new Spotify({
   id: "b660b9d15cf64d169086feeee9c25eaa",
   secret: "6b27fc3404eb4d0a91648c6870b7e361"
 });
  
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+spotify.search({ type: 'track', query: search }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
@@ -20,4 +37,6 @@ console.log(data);
 });
 
 // above /\ this code works!!
+
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
