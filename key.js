0c255620148f2aa5d4fd2c840022b548e87ec630
jon <jcrossguitar@gmail.com>
2019-09-04 15:53:02 -0700

    I managed to get the spotify api to work correctly.

--- key.js
+++ key.js
@@ -27,6 +27,11 @@ var spotify = new Spotify({
   id: "b660b9d15cf64d169086feeee9c25eaa",
   secret: "6b27fc3404eb4d0a91648c6870b7e361"
 });
+
+var getArtistNames = function (artist) {
+    return artist.name;
+}
+
  
 spotify
     .search({ type: 'track', query: search }, function(err, data) {
@@ -35,62 +40,20 @@ spotify
   }
  
 console.log(data); 
+
+var songs = data.tracks.items;
+for (var i = 0; i < songs.length; i++) {
+    console.log(i);
+    console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+    console.log('song name: ' + songs[i].name);
+    console.log('preview song: ' + songs[i].preview_url);
+    console.log('album: ' + songs[i].album.name);
+    console.log('----------------------------------------------------------');
+}
 });
 
 // above /\ this code works!!
 
 // var concertUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-
-
-var axios = require("axios");
-
-// Store all of the arguments in an array
-var nodeArgs = process.argv;
-
-// Create an empty variable for holding the movie name
-var movieName = "";
-
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
-
-  if (i > 2 && i < nodeArgs.length) {
-    movieName = movieName + "+" + nodeArgs[i];
-  } else {
-    movieName += nodeArgs[i];
-
-  }
-}
-
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=8de6eb8b";
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-axios.get(queryUrl).then(
-  function(response) {
-    console.log("Release Year: " + response.data.Year);
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
 
 
