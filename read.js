b091ef72ac6b998c6f112abe0a495d68fe3b61c8
jon <jcrossguitar@gmail.com>
2019-11-23 10:24:28 -0800

    pictures added

---
+++ read.js
@@ -0,0 +1,7 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId: 'b660b9d15cf64d169086feeee9c25eaa',
+  clientSecret: '6b27fc3404eb4d0a91648c6870b7e361',
+});
